26be15c93fa46f0ad3a9afec71fa58c201a88070
Fabian <fabian.dl@mahertek.com>
2020-07-10 14:59:58 -0300

    new update 10-07

--- SuiteScripts/Wizard-SU/OC_Search_license_data_renewal-SU.js
+++ SuiteScripts/Wizard-SU/OC_Search_license_data_renewal-SU.js
@@ -5,7 +5,7 @@
  * 
  */
 
-define(['N/https', 'N/record', 'N/search'], function(https, record, search) {
+define(['N/https', 'N/record', 'N/search'], function (https, record, search) {
 
 
 
@@ -31,7 +31,7 @@ define(['N/https', 'N/record', 'N/search'], function(https, record, search) {
         return objLicence;
     }
 
-    function searchLicence(context,invoiceRcd, invoice) {
+    function searchLicence(context, invoiceRcd, invoice) {
 
         // log.audit('getFunction', 'Context object : ' +
         // JSON.stringify(context));
@@ -40,37 +40,38 @@ define(['N/https', 'N/record', 'N/search'], function(https, record, search) {
             if (invoiceRcd) {
                 //log.audit('showSearchPage', 'Context parameters : ' + JSON.stringify(params));
 
-               
+
 
                 var invoiceId = invoice;
                 var licenseId = invoiceRcd.getValue('createdfrom');
                 var license = invoiceRcd.getText('createdfrom');
-                var customerId=invoiceRcd.getValue('entity');
+                var customerId = invoiceRcd.getValue('entity');
 
-
+                log.audit('invoiceRcd', invoiceRcd);
                 if (licenseId) {
 
                     var obj = lookUpLicenses(licenseId);
-                    objData.customerId=customerId;
                     objData.name = obj.businessName;
                     objData.license = license;
                     objData.location = obj.location;
-                    objData.invoice = invoiceId
-
+                    
                 }
+                objData.customerId = customerId;
+                objData.invoice = invoiceId
+
             }
         } catch (e) {
             log.audit('Error', 'searchLicence: ' + JSON.stringify(e));
         }
-       
-        context.response.write(JSON.stringify(objData));
 
+        // context.response.write(JSON.stringify(objData));
+       return objData
     }
 
     function onRequestFxn(context) {
 
         try {
-            
+            var message = '';
             var params = context.request.parameters;
             log.audit('onRequestFxn', 'Context parameters : ' + JSON.stringify(params));
             var invId = params.invId;
@@ -80,14 +81,34 @@ define(['N/https', 'N/record', 'N/search'], function(https, record, search) {
                 id: invId
             });
 
-
-            searchLicence(context,invoiceRcd, invId);
-
+            if(invoiceRcd){
+            var dateCompare = '06/01/' + new Date().getFullYear();
+            dateCompare = Date.parse(dateCompare);
+            var date = invoiceRcd.getValue('trandate');
+            date = Date.parse(date)
+            var amauntDue = invoiceRcd.getValue('amountremainingtotalbox');
+            log.audit('invoiceRcd', invoiceRcd);
+        var objRespose;
+        log.audit('parseFloat(amauntDue)', parseFloat(amauntDue));
+            if (dateCompare < date && parseFloat(amauntDue) > 0) objRespose=searchLicence(context, invoiceRcd, invId);
+            if (dateCompare > date) message = 'The Renewal ID you entered is not valid for this fiscal year.'
+            if (parseFloat(amauntDue) == 0) message ='The Renewal ID you entered is not valid for this fiscal year or has already been paid. Please refer to your records or contact the City (link to Contact Us page) with any additional questions'
+            }
+             if(objRespose){
+                log.audit('invoiceRcd', JSON.stringify(objRespose));
+                context.response.write(JSON.stringify(objRespose));
+             }else{
+                context.response.write(JSON.stringify({ message:message}));
+             } 
+             
         } catch (e) {
             log.error('onRequestFxn', 'ERROR : ' + e);
-          context.response.write(JSON.stringify({}));
+            log.audit('msge', 'message: There are no results for this Renewal ID. Try again.');
+            context.response.write(JSON.stringify({ message: 'There are no results for this Renewal ID. Try again.' }));
         }
+       
     }
+
     return {
         onRequest: onRequestFxn
     };
